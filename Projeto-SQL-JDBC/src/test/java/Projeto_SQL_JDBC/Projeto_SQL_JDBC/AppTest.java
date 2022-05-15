39449a48d90b05d177f0f80a416cf04ccbf96016
lucas gomes silava <lucasgomessilva713@gmail.com>
2022-05-14 23:48:30 -0300

    criaçao do projeto SQL com JDBC...

---
+++ Projeto-SQL-JDBC/src/test/java/Projeto_SQL_JDBC/Projeto_SQL_JDBC/AppTest.java
@@ -0,0 +1,20 @@
+package Projeto_SQL_JDBC.Projeto_SQL_JDBC;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
