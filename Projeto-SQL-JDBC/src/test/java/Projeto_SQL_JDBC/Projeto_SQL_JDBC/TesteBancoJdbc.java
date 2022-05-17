4c5c7823a443f9b938dd22e1d464b2ffac07edf5
lucas gomes silava <lucasgomessilva713@gmail.com>
2022-05-17 00:44:02 -0300

    Criando conexão com o banco de dados

---
+++ Projeto-SQL-JDBC/src/test/java/Projeto_SQL_JDBC/Projeto_SQL_JDBC/TesteBancoJdbc.java
@@ -0,0 +1,15 @@
+package Projeto_SQL_JDBC.Projeto_SQL_JDBC;
+
+import org.junit.Test;
+
+import conexaojdbc.SingleConnection;
+
+public class TesteBancoJdbc{
+	
+	
+	@Test
+	public void initBanco() {
+		SingleConnection.getConnection();
+	}
+
+}
