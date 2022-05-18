92c3a6b124ac1cce46539c5e0f524264f46e76b6
lucas gomes silava <lucasgomessilva713@gmail.com>
2022-05-18 15:57:34 -0300

    Criando INSERT ao banco de dados

--- Projeto-SQL-JDBC/src/main/java/dao/UserposDAO.java
+++ Projeto-SQL-JDBC/src/main/java/dao/UserposDAO.java
@@ -1,15 +1,43 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import conexaojdbc.SingleConnection;
+import model.Userposjava;
 
 public class UserposDAO {
-	
+
 	private Connection connection;
-	
+
 	public UserposDAO() {
 		connection = SingleConnection.getConnection();
+	}
+
+	//metodo para salvar no banco 
+	public void Salvar(Userposjava userposjava) {
+		try {
+			//sql para inser no banco
+			String sql = "insert into userposjava (id, nome, email) values (?, ?, ?)";
+			PreparedStatement insert = connection.prepareStatement(sql);//preparando o sql
+			
+			//passano os paramentros segindo as ordem
+			insert.setLong(1, userposjava.getId());
+			insert.setString(2, userposjava.getNome());
+			insert.setString(3, userposjava.getEmail());
+			
+			insert.execute();//executando 
+			connection.commit();//salvando no banco
+
+		} catch (Exception e) {
+			try {
+				connection.rollback();// reverte operaçao se tiver erro
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			e.printStackTrace();
+		}
 	}
 
 }
--- Projeto-SQL-JDBC/src/test/java/Projeto_SQL_JDBC/Projeto_SQL_JDBC/TesteBancoJdbc.java
+++ Projeto-SQL-JDBC/src/test/java/Projeto_SQL_JDBC/Projeto_SQL_JDBC/TesteBancoJdbc.java
@@ -3,6 +3,8 @@ package Projeto_SQL_JDBC.Projeto_SQL_JDBC;
 import org.junit.Test;
 
 import conexaojdbc.SingleConnection;
+import dao.UserposDAO;
+import model.Userposjava;
 
 public class TesteBancoJdbc{
 	
@@ -10,6 +12,15 @@ public class TesteBancoJdbc{
 	@Test
 	public void initBanco() {
 		SingleConnection.getConnection();
+		
+		UserposDAO userposDAO = new UserposDAO();
+		Userposjava userposjava = new Userposjava();
+		
+		userposjava.setId(6L);
+		userposjava.setNome("paulo");
+		userposjava.setEmail("paulo@gmail.com");
+		
+		userposDAO.Salvar(userposjava);
 	}
  
 }
